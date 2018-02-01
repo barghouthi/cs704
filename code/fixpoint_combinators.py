1f2b4f489067f269daaf2ae5dc0b744ef8e8f3b4
Aws Albarghouthi <albargah@gmail.com>
2018-02-01 10:16:39 -0600

    fixpoint code

---
+++ code/fixpoint_combinators.py
@@ -0,0 +1,33 @@
+U = lambda x: x(x)
+fact = lambda h: lambda x: 1 if x == 0 else x * (h (h))(x-1)
+factu = U (fact)
+
+# doesn't work yet
+Y = lambda F: F (Y (F))
+# Y(lambda f: lambda n: 1 if n <= 0 else n*f(n-1))
+
+# now this works, but it's recursive
+Y = lambda F: F(lambda x:Y(F)(x))
+# Y(lambda f: lambda n: 1 if n <= 0 else n*f(n-1))(5)
+
+# look, no recursion!
+Y = U(lambda h: lambda F: F(lambda x:U(h)(F)(x)))
+
+Y = ((lambda h: lambda F: F(lambda x:h(h)(F)(x))) (lambda h: lambda F: F(lambda x:h(h)(F)(x))))
+
+# fibonacci
+fib = lambda f: lambda n: n if n==0 or n == 1 else f(n-1) + f(n-2)
+
+# caching y combinator
+def Ymem(F, cache=None):
+    if cache is None:
+        cache = {}
+
+    def fun(arg):
+        if arg in cache:
+            return cache[arg]
+
+        res = (F(lambda n: (Ymem(F,cache))(n)))(arg)
+        cache[arg] = res
+        return res
+    return fun
